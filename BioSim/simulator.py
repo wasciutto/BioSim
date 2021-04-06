93bd857b3177712bcb6e31a27acf356ee12a523d
Will Asciutto <wjasciutto@gmail.com>
2021-04-06 00:01:18 -0400

    Add logging to simulator

--- BioSim/simulator.py
+++ BioSim/simulator.py
@@ -2,6 +2,7 @@ import random
 import math
 import configparser
 import csv
+import logging
 
 from enum import Enum
 
@@ -132,6 +133,8 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read('sim_config.ini')
 
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(asctime)s %(message)s')
+    log = logging.getLogger(__name__)
 
     cVeg = 0
     cDays = 0
@@ -140,8 +143,9 @@ if __name__ == "__main__":
         writer = csv.writer(file)
 
         writer.writerow(["Test Number", "Days Survived", "Max Vegetation"])
-        
-        for x in range(0, 50):    
+
+        log.info("Simulation Started")
+        for x in range(0, 50):
             environ = EnvironGenerator(int(config['DEFAULT']['LIGHT_RAIN_CHANCE']),
                                        int(config['DEFAULT']['HEAVY_RAIN_CHANCE']))
             bio1 = BioArea(environ,
@@ -155,19 +159,20 @@ if __name__ == "__main__":
                            float(config['DEFAULT']['LONG_DROUGHT_MULTIPLIER']),
                            float(config['DEFAULT']['SHORT_DROUGHT_MULTIPLIER'])
                            )
-            print(environ)
-            print(bio1)
+            log.debug(environ)
+            log.debug(bio1)
             while(bio1.vegetation > 1):
                 environ.next_day()
                 bio1.next_day()
-                print(environ)
-                print(bio1)
+                log.debug(environ)
+                log.debug(bio1)
             writer.writerow([bio1.areaNumber, str(environ.day), str(bio1.maxVegetation)])
             
             cVeg += bio1.maxVegetation
             cDays += environ.day
-        
-    print("Average Max Vegetation Density: " + str(cVeg / 100))
-    print("Average Number of Days: " + str(cDays / 100))
+
+    log.info("Simulation Complete")
+    log.info("Average Max Vegetation Density: " + str(cVeg / 100))
+    log.info("Average Number of Days: " + str(cDays / 100))
     
     
