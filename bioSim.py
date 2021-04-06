edd53faa42220c5ca1402150eb0eced80d2749da
Will Asciutto <wjasciutto@gmail.com>
2021-04-05 23:30:47 -0400

    Clean-up style

--- bioSim.py
+++ bioSim.py
@@ -4,7 +4,7 @@ import configparser
 
 from enum import Enum
 
-class Precipitation(Enum):
+class PrecipLevel(Enum):
     NONE = 0
     LIGHT = 1
     HEAVY = 2
@@ -15,32 +15,32 @@ class EnvironGenerator:
     def __init__(self, light_rain_chance, heavy_rain_chance):
         self.light_rain_chance = light_rain_chance
         self.heavy_rain_chance = heavy_rain_chance
-        self.precipLevel = self.genPrecipLevel()
+        self.precipLevel = self.gen_precip_level()
         self.day = 0
     
     def __str__(self):
         return "Day: " + str(self.day) \
         + "\nPrecipitation Level: " + str(self.precipLevel) + "\n"
     
-    def nextDay(self):
-        self.precipLevel = self.genPrecipLevel()
+    def next_day(self):
+        self.precipLevel = self.gen_precip_level()
         self.day += 1
     
     #0 indicates no precip, 1 indicates light precip, 2 indicates heavy precip
-    def genPrecipLevel(self):
+    def gen_precip_level(self):
         odds = random.randint(0,100)
-        if (odds >= self.light_rain_chance and odds < self.heavy_rain_chance):
-            return Precipitation.LIGHT
-        elif (odds >= self.heavy_rain_chance):
-            return Precipitation.HEAVY
+        if self.light_rain_chance <= odds < self.heavy_rain_chance:
+            return PrecipLevel.LIGHT
+        elif odds >= self.heavy_rain_chance:
+            return PrecipLevel.HEAVY
         else:
-            return Precipitation.NONE
+            return PrecipLevel.NONE
     
 
 class BioArea:
     areaNumber = 0
     
-    def __init__(self, environ, starting_water_level, starting_veg_level, flat_evaporation, light_rain_amount,
+    def __init__(self, environment, starting_water_level, starting_veg_level, flat_evaporation, light_rain_amount,
                  heavy_rain_amount, long_drought_days, long_drought_multiplier, short_drought_days,
                  short_drought_multiplier):
         self.waterLevel = starting_water_level
@@ -53,45 +53,45 @@ class BioArea:
         self.short_drought_days = short_drought_days
         self.short_drought_multiplier = short_drought_multiplier
         BioArea.areaNumber += 1
-        self.environ = environ
+        self.environment = environment
         self.vegeplier = 0
         self.maxVegetation = 0
         self.droughtStreak = 0
     
     #call order for bio area simulation actions
-    def nextDay(self):
+    def next_day(self):
         
-        self.drainWater()    
-        self.checkPrecip()
-        self.checkDroughtStreak()     
-        self.setVegeplier()
-        self.setVegetation()
+        self.drain_water()
+        self.check_precip()
+        self.check_drought_streak()
+        self.set_vegeplier()
+        self.set_vegetation()
         
         #Record maximum vegetation value
         if (self.vegetation > self.maxVegetation):
             self.maxVegetation = self.vegetation
     
     #removes water based on water absorption factors
-    def drainWater(self):
+    def drain_water(self):
         #Standard evaporation
-        self.lowerWater(self.flat_evaporation)
+        self.lower_water(self.flat_evaporation)
         
         #Vegetation absorption formula
-        absorptionRate = round(self.vegetation * (1/100), 2)
-        self.lowerWater(absorptionRate)
+        absorption_rate = round(self.vegetation * (1/100), 2)
+        self.lower_water(absorption_rate)
     
     #increases water level based on precipitation
-    def checkPrecip(self):
-        if(environ.precipLevel == Precipitation.LIGHT):
-            self.raiseWater(self.light_rain_amount)
-        elif(environ.precipLevel == Precipitation.HEAVY):
-            self.raiseWater(self.heavy_rain_amount)
+    def check_precip(self):
+        if environ.precipLevel == PrecipLevel.LIGHT:
+            self.raise_water(self.light_rain_amount)
+        elif environ.precipLevel == PrecipLevel.HEAVY:
+            self.raise_water(self.heavy_rain_amount)
     
-    def setVegeplier(self):
+    def set_vegeplier(self):
         #drought multiplier (overrides vegetation growth formula)
-        if(self.droughtStreak > self.long_drought_days):
+        if self.droughtStreak > self.long_drought_days:
             self.vegeplier = self.long_drought_multiplier
-        elif(self.droughtStreak > self.short_drought_days):
+        elif self.droughtStreak > self.short_drought_days:
             self.vegeplier = self.short_drought_multiplier
         #vegetation growth formula
         #vegeplier ranges from .75 to 1.25
@@ -99,23 +99,23 @@ class BioArea:
             self.vegeplier = 1 + round(((self.waterLevel - 50) * (1/200)),2)
     
     #increases drought streak by 1 if water level is 0
-    def checkDroughtStreak(self):
-        if(self.waterLevel == 0):
+    def check_drought_streak(self):
+        if self.waterLevel == 0:
             self.droughtStreak += 1
         else:
             self.droughtStreak = 0    
     
-    def lowerWater(self, amount):
+    def lower_water(self, amount):
         self.waterLevel = round(self.waterLevel - amount, 2)
-        if (self.waterLevel < 0):
+        if self.waterLevel < 0:
             self.waterLevel = 0
             
-    def raiseWater(self, amount):
+    def raise_water(self, amount):
         self.waterLevel = round(self.waterLevel + amount, 2)
-        if (self.waterLevel > 100):
+        if self.waterLevel > 100:
             self.waterLevel = 100
     
-    def setVegetation(self):
+    def set_vegetation(self):
         self.vegetation = math.floor(self.vegeplier * self.vegetation)
         #if (self.vegetation > 1000):
         #    self.vegetation = 1000
@@ -155,8 +155,8 @@ if __name__ == "__main__":
             print(environ)
             print(bio1)
             while(bio1.vegetation > 1):
-                environ.nextDay()
-                bio1.nextDay()
+                environ.next_day()
+                bio1.next_day()
                 print(environ)
                 print(bio1)
             line = str(bio1.areaNumber) + "," + str(environ.day) + "," + str(bio1.maxVegetation) + "\n"
